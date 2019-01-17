8d07d202134b1e38a436f5cf82a16927430e8b22
Sam Yu <smyu03@gmail.com>
2019-01-16 22:12:30 -0500

    new build

---
+++ build/precache-manifest.8fc3ff71e6c3ed284d724e30397bef9f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "468d1e43a1ec8b48f3138b4daae1d7bf",
+    "url": "/static/media/webveloper.468d1e43.jpg"
+  },
+  {
+    "revision": "6db6c0414063603a5acdf80837822529",
+    "url": "/static/media/samsung.6db6c041.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "54a6ee22600ebc1d7482",
+    "url": "/static/js/main.54a6ee22.chunk.js"
+  },
+  {
+    "revision": "ae4323a5073c2769f4fc",
+    "url": "/static/js/1.ae4323a5.chunk.js"
+  },
+  {
+    "revision": "54a6ee22600ebc1d7482",
+    "url": "/static/css/main.19a03175.chunk.css"
+  },
+  {
+    "revision": "ae4323a5073c2769f4fc",
+    "url": "/static/css/1.b7a3ac5f.chunk.css"
+  },
+  {
+    "revision": "99e052f4f7636795a34aa016fda823df",
+    "url": "/index.html"
+  }
+];
